16cd55ff22dd81dd3b41791cad8760ff91d6daa2
cory.leach@gmail.com <cory.leach@gmail.com>
2019-08-30 16:35:23 -0400

    Broken awaiters test

---
+++ Runtime/Awaiters.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Gameframe.Async
+{
+    public static class Awaiters
+    {
+        private static readonly WaitForBackground _waitForBackground = new WaitForBackground();
+        private static readonly WaitForUnityThread _waitForUnityThread = new WaitForUnityThread();
+        
+        public static WaitForBackground BackgroundThread => _waitForBackground;
+        public static WaitForUnityThread MainThread => _waitForUnityThread;
+    }
+
+    public class WaitForBackground
+    {
+        public ConfiguredTaskAwaitable.ConfiguredTaskAwaiter GetAwaiter()
+        {
+            return Task.Run(() => {}).ConfigureAwait(false).GetAwaiter();
+        }
+    }
+
+    public class WaitForUpdate : CustomYieldInstruction
+    {
+        public override bool keepWaiting => false;
+    }
+
+    public class WaitForUnityThread
+    {
+        public ConfiguredTaskAwaitable.ConfiguredTaskAwaiter GetAwaiter()
+        {
+            var task = Task.Factory.StartNew(() => {}, CancellationToken.None, TaskCreationOptions.None, UnityTaskUtil.UnityTaskScheduler);
+            return task.ConfigureAwait(false).GetAwaiter();
+        }
+    }
+    
+}
+
+
---
+++ Tests/Runtime/AwaitersTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Gameframe.Async;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Gameframe.Async.Tests
+{
+    public class AwaitersTests
+    {
+        [UnityTest]
+        public IEnumerator TestBackgroundAndMainThreadAwaiters()
+        {
+            var task = DoTest();
+            yield return task.AsIEnumerator();
+        }
+
+        private async Task DoTest()
+        {
+            //Start on unity thread
+            Assert.IsTrue(UnityTaskUtil.CurrentThreadIsUnityThread);
+            await Awaiters.BackgroundThread;
+            Assert.IsFalse(UnityTaskUtil.CurrentThreadIsUnityThread);
+            await Awaiters.MainThread;
+            Assert.IsTrue(UnityTaskUtil.CurrentThreadIsUnityThread);
+        }
+        
+    }
+}
