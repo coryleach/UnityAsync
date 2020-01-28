a47dee74b6ab5b34d86d8d82609a64d3cd311726
Cory Leach <cory.leach@gmail.com>
2020-01-28 11:03:57 -0500

    Update CoroutineRunner.cs

--- Runtime/Coroutines/CoroutineRunner.cs
+++ Runtime/Coroutines/CoroutineRunner.cs
@@ -5,8 +5,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Gameframe.Async.Coroutines
 {
+    /// <summary>
+    /// Static helper class for running coroutines independently of MonoBehaviour
+    /// Coroutines always run on the main thread.
+    /// </summary>
     public static class CoroutineRunner
     {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -15,20 +23,48 @@ namespace Gameframe.Async.Coroutines
             UnitySynchronizationContext = SynchronizationContext.Current;
             Application.quitting += OnApplicationQuitting;
         }
-    
+        
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        private static void EditorInstall()
+        {
+            EditorApplication.playModeStateChanged += EditorApplicationOnPlayModeStateChanged;   
+        }
+
+        private static void EditorApplicationOnPlayModeStateChanged(PlayModeStateChange obj)
+        {
+            //I do not intend to support editor coroutines
+            //Do I need to cancel coroutines on play mode change?
+        }
+#endif   
+        
         private static SynchronizationContext UnitySynchronizationContext { get; set; }
         private static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         
+        /// <summary>
+        /// Use this method when you want to await coroutine completion.
+        /// Coroutine itself will always run on the main thread.
+        /// </summary>
+        /// <param name="routine">coroutine to run</param>
+        /// <returns>awaitable task</returns>
         public static async Task RunAsync(IEnumerator routine)
         {
             await UnityTaskUtil.RunOnUnityThreadAsync(() => Run(routine,cancellationTokenSource.Token) );
         }
         
+        /// <summary>
+        /// Use this method to start a coroutine from any thread.
+        /// Coroutines themselves always run on the main thread
+        /// </summary>
+        /// <param name="enumerator">coroutine to run</param>
         public static void Start(IEnumerator enumerator)
         {
             UnityTaskUtil.RunOnUnityThread(() => Run(enumerator,cancellationTokenSource.Token));
         }
     
+        /// <summary>
+        /// Stop all coroutines that have been started with CoroutineRunner
+        /// </summary>
         public static void StopAll()
         {
             cancellationTokenSource.Cancel();
@@ -55,7 +91,7 @@ namespace Gameframe.Async.Coroutines
             var processStack = new Stack<IEnumerator>();
             processStack.Push((IEnumerator)state);
     
-            while ( processStack.Count > 0)
+            while (processStack.Count > 0)
             {
                 var currentCoroutine = processStack.Peek();
                 var done = false;
